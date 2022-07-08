6cfa3a93787bfe1c0372414b502ec123cc35406c
levy_quilli <lquilliam@deloitte.com.au>
2022-07-08 07:34:01 +0100

    lecture 10 - string methods

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-console.log(1);
-console.log(2);
+//COMMON STRING METHODS
+
+let email = "levi@gmail.com";
+console.log(email.lastIndexOf("m"));
+console.log(email.slice(0, 10)); //string[:10] <- in python
+console.log(email.substring(2, 5)); //Starts at index 2 and get 5 characters
+console.log(email.replace("@", "&")); //Replaces the first instance
