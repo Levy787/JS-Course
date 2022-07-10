78e2a44eb6715ea9bca2bae1098c379fe6c589a0
levy_quilli <lquilliam@deloitte.com.au>
2022-07-10 11:06:53 +0100

    Commented out content

--- script.js
+++ script.js
@@ -4,48 +4,50 @@
 
 //SELECTING ELEMENTS
 
-//This will grab the first p tag
-const para = document.querySelector("p");
+// //This will grab the first p tag
+// const para = document.querySelector("p");
 
-const err = document.querySelector(".error");
-const err2 = document.querySelector("div.error");
+// const err = document.querySelector(".error");
+// const err2 = document.querySelector("div.error");
 
-const paras = document.querySelectorAll("p");
-const errors = document.querySelectorAll(".error");
+// const paras = document.querySelectorAll("p");
+// const errors = document.querySelectorAll(".error");
 
-const title = document.getElementById("page-title");
-const errors_2 = document.getElementsByClassName("error");
-const ps = document.getElementsByTagName("p");
+// const title = document.getElementById("page-title");
+// const errors_2 = document.getElementsByClassName("error");
+// const ps = document.getElementsByTagName("p");
 
-//ADDING AND CHANGING CONTENT
+// //ADDING AND CHANGING CONTENT
 
-console.log(para.innerText);
-para.innerText = "Ninjas are awersome!";
+// console.log(para.innerText);
+// para.innerText = "Ninjas are awersome!";
 
-const content = document.querySelector(".content");
-content.innerHTML = "<h2>This is a new h2 tag</h2>";
+// const content = document.querySelector(".content");
+// content.innerHTML = "<h2>This is a new h2 tag</h2>";
 
-const people = ["Mario", "Luigi", "Yoshi"];
+// const people = ["Mario", "Luigi", "Yoshi"];
 
-people.forEach((person) => {
-  content.innerHTML += `<p>${person}`;
-});
+// people.forEach((person) => {
+//   content.innerHTML += `<p>${person}`;
+// });
 
-//CHANGING element attributes
-const link = document.querySelector("a");
-console.log(link.getAttribute("href"));
-link.setAttribute("href", "https://www.youtube.com");
-link.innerText = "Youtube";
+// //CHANGING element attributes
+// const link = document.querySelector("a");
+// console.log(link.getAttribute("href"));
+// link.setAttribute("href", "https://www.youtube.com");
+// link.innerText = "Youtube";
 
-const msg = document.querySelector("p.error");
-msg.setAttribute("error", "success");
+// const msg = document.querySelector("p.error");
+// msg.setAttribute("error", "success");
 
-//Adding attributes
-msg.setAttribute("style", "color: green");
+// //Adding attributes
+// msg.setAttribute("style", "color: green");
 
-const h2 = document.querySelector("h2.second-h2");
-h2.style.margin = "50px";
-h2.style.fontSize = "60px";
-h2.style.margin = ""; //Remove property
+// const h2 = document.querySelector("h2.second-h2");
+// h2.style.margin = "50px";
+// h2.style.fontSize = "60px";
+// h2.style.margin = ""; //Remove property
 
 //Adding or removing classes
+const content = document.querySelector("p");
+console.log(content.classList);
